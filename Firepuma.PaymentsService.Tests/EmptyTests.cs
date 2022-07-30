2f229e402cdd68c0f66e60d094ed7b05b3d88e4c
Francois Hill wootbook <francoishill11@gmail.com>
2022-07-30 11:09:38 +0200

    feat: add workflow to publish Abstractions and Client nuget packages
    Adding an empty test project, mainly to lay the foundation for future tests

---
+++ Firepuma.PaymentsService.Tests/EmptyTests.cs
@@ -0,0 +1,11 @@
+namespace Firepuma.PaymentsService.Tests;
+
+public class EmptyTests
+{
+    [Fact]
+    public void EmptyTest()
+    {
+        //TODO: Add tests
+        Assert.True(true);
+    }
+}

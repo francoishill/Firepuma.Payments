46ca0cd2a171e7ca4038132c99e2635508a5c4b3
Francois Hill wootbook <francoishill11@gmail.com>
2022-08-24 07:16:23 +0200

    fix: ignore get-only properties in `IPaymentApplicationConfig`

--- Firepuma.Payments.Implementations/Config/IPaymentApplicationConfig.cs
+++ Firepuma.Payments.Implementations/Config/IPaymentApplicationConfig.cs
@@ -1,11 +1,16 @@
-﻿using Azure.Data.Tables;
+﻿using System.Runtime.Serialization;
+using Azure.Data.Tables;
 using Firepuma.Payments.Abstractions.ValueObjects;
 
 namespace Firepuma.Payments.Implementations.Config;
 
 public interface IPaymentApplicationConfig : ITableEntity
 {
+    [IgnoreDataMember]
     public PaymentGatewayTypeId GatewayTypeId { get; }
+
+    [IgnoreDataMember]
     public ClientApplicationId ApplicationId { get; }
+
     public string ApplicationSecret { get; set; }
 }
